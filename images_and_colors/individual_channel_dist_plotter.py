2cd20bc6be9d58d76fbb9cb8518573ab2d9b88af
DebasishDhal <31160148+DebasishDhal@users.noreply.github.com>
2023-11-27 23:53:01 +0530

    Create individual_channel_dist_plotter.py

---
+++ images_and_colors/individual_channel_dist_plotter.py
@@ -0,0 +1,27 @@
+#Plot the distribution of pixel values in all the 3 channels, alongside the original image.
+def channel_distribution_plotter(img_array):
+
+    img_array = img_array[:,:,:3] #Not considering the A channel, if it's a RGBA image.
+
+    plt.subplot(2,2,1)
+    plt.hist(img_array[:,:,0].ravel(),bins=256,color='red');
+    plt.title("Red Channel")
+
+    plt.subplot(2,2,2)
+    plt.hist(img_array[:,:,1].ravel(),bins=256,color='green');
+    plt.title("Green Channel")
+
+    plt.subplot(2,2,3)
+    plt.hist(img_array[:,:,2].ravel(),bins=256,color='blue');
+    plt.title("Blue Channel")
+
+    plt.subplot(2,2,4)
+    plt.imshow(cv2.cvtColor(img_array,cv2.COLOR_BGR2RGB))
+    plt.title("Original Image")
+
+    plt.suptitle("Pixel values distribution in each channel\nx-axis: pixel values, y-axis: number of pixels")
+    plt.tight_layout()
+
+    plt.show()
+
+channel_distribution_plotter(img)
