84c533bc8de1810963d7a5bdacdae7ce7268cb37
DebasishDhal <31160148+DebasishDhal@users.noreply.github.com>
2023-12-18 00:14:45 +0530

    Update activate.bat

--- automated_leetcode_rank_retrieval/activate.bat
+++ automated_leetcode_rank_retrieval/activate.bat
@@ -1,1 +1,11 @@
+::First, we set up the virtual environment in which the script will run. Then we run the python script
+set original_dir=%CD%
+set venv_root_dir=C:\Users\HP\anaconda3
+cd %venv_root_dir%
+call %venv_root_dir%\Scripts\activate.bat
 
+python C:\Users\HP\OneDrive\Desktop\Python\Pet_Projects\leetcode_rank_retrieval\script.py
+
+call %venv_root_dir%\Scripts\deactivate.bat
+cd %original_dir%
+exit /B 1
