e1444e554c45e1db82502d0e0e22de4821118576
DebasishDhal <31160148+DebasishDhal@users.noreply.github.com>
2024-01-26 17:37:12 +0530

    Update main.py

--- video_shuffling/main.py
+++ video_shuffling/main.py
@@ -1,6 +1,6 @@
-! wget https://repo.anaconda.com/miniconda/Miniconda3-py37_4.8.2-Linux-x86_64.sh
-! chmod +x Miniconda3-py37_4.8.2-Linux-x86_64.sh
-! bash ./Miniconda3-py37_4.8.2-Linux-x86_64.sh -b -f -p /usr/local
+!wget https://repo.anaconda.com/miniconda/Miniconda3-py37_4.8.2-Linux-x86_64.sh
+!chmod +x Miniconda3-py37_4.8.2-Linux-x86_64.sh
+!bash ./Miniconda3-py37_4.8.2-Linux-x86_64.sh -b -f -p /usr/local
 import sys
 sys.path.append('/usr/local/lib/python3.7/site-packages/')
 !conda install -c conda-forge ffmpeg -y
